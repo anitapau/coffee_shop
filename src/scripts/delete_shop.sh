3ae8fd527611f6fd9b9621df80bf0ae230a9a512
Thanh Le <thanh07@uw.edu>
2017-02-15 19:36:49 -0800

    Update delete_shop.sh

--- src/scripts/delete_shop.sh
+++ src/scripts/delete_shop.sh
@@ -1,1 +1,13 @@
 
+ENDPOINT=http://gentle-coast-59786.herokuapp.com/tcss360/coffeeshop
+# heroku
+#ENDPOINT=https://frozen-temple-15527.herokuapp.com/tcss360/coffeeshop
+# local web runner deploy
+ENDPOINT=http://localhost:8080/tcss360/coffeeshop
+# net beans deploy
+#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/coffeeshop
+# manual deploy
+#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/coffeeshop
+curl -X POST -H "Content-Type: application/json" -d @./fred.json $ENDPOINT
+#curl -X POST -H "Content-type: application/json" -d '{"name":"Fred Smith"}' $ENDPOINT
+echo
