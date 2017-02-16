75fa3fb1c939c08a3f48560d6fc93347da9fb367
Michael Anthony <mike365@uw.edu>
2017-02-15 19:55:52 -0800

    updadte ):<

--- src/scripts/create_review.sh
+++ src/scripts/create_review.sh
@@ -8,6 +8,6 @@ ENDPOINT=http://localhost:8080/tcss360/cofeeshop
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/
 # manual deploy
 #ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/users
-curl -X POST -H "Content-Type: application/json" -d @./review.json $ENDPOINT
+curl -X POST -H "Content-Type: application/json" -d @./review.json_create $ENDPOINT
 #curl -X POST -H "Content-type: application/json" -d '{"name": "Michael", "reivew": "sucks ass", "rating": -1}' $ENDPOINT
 echo
