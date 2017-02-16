24ff622ec654606a08eed1f1d60964ad5a599853
Michael Anthony <mike365@uw.edu>
2017-02-15 20:00:51 -0800

    update

--- src/scripts/get_review.sh
+++ src/scripts/get_review.sh
@@ -1,5 +1,5 @@
-
-ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/review# local web runner deploy
+ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/review
+#local web runner deploy
 #ENDPOINT=http://localhost:8080/tcss360/users
 # net beans deploy
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/users
