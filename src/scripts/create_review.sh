6321f9cae5d8a6de98bb61d6f2787ad4c5e69f7f
anitapau <anitapau@uw.edu>
2017-03-01 15:03:20 -0800

    Create create_review.sh

---
+++ src/scripts/create_review.sh
@@ -0,0 +1,16 @@
+# heroku
+ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/review
+# local web runner deploy
+#ENDPOINT=http://localhost:8080/tcss360/review
+# net beans deploy
+#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/review
+# manual deploy
+#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/review
+
+curl -X POST -H "Content-Type: application/json" -d '{
+	"shopid": 1,
+  "review": "very bad",
+  "rating": 1
+}' $ENDPOINT
+
+echo
