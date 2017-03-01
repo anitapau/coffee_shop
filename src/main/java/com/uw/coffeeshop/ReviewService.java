62ba278c9baaf5e246c5ddc369b6b4213f69aad8
Thanh Le <thanh07@uw.edu>
2017-02-28 23:28:51 -0800

    Update ReviewService.java

--- src/main/java/com/uw/coffeeshop/ReviewService.java
+++ src/main/java/com/uw/coffeeshop/ReviewService.java
@@ -38,123 +38,122 @@ public class ReviewService {
     @Context
     private UriInfo context;
 
-//    @GET
-//    @Path("/api/review")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public String getCoffeeShop() throws IOException {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("<html><body><style>table, th, td {font-family:Arial,Verdana,sans-serif;font-size:16px;padding: 0px;border-spacing: 0px;}</style><b>MESSAGE LIST:</b><br><br><table cellpadding=10 border=1><tr><td>`name`</td><td>reviewId</td><td>shopId</td><td>rating</td></tr>");
-//        try {
-//            Model db = Model.singleton();
-//            Review[] review = db.getReview();
-//            for (int i = 0; i < review.length; i++) {
-//                sb.append("<tr><td>" + review[i].getReview() + "</td><td>" + review[i].getReviewId() + "</td><td>" + review[i].getShopid() + "</td><td>" + review[i].getRating() + "</td></tr>");
-//            }
-//        } catch (Exception e) {
-//            sb.append("</table><br>Error getting review: " + e.toString() + "<br>");
-//        }
-//        sb.append("</table></body></html>");
-//        return sb.toString();
-//
-//    }
+    @GET
+    @Path("/api/review")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getReview() throws IOException {
+       StringBuilder sb = new StringBuilder();
+        sb.append("<html><body><style>table, th, td {font-family:Arial,Verdana,sans-serif;font-size:16px;padding: 0px;border-spacing: 0px;}</style><b>MESSAGE LIST:</b><br><br><table cellpadding=10 border=1><tr><td>`name`</td><td>reviewId</td><td>shopId</td><td>rating</td></tr>");
+        try {
+            Model db = Model.singleton();
+            Review[] review = db.getReview();
+            for (int i = 0; i < review.length; i++) {
+                sb.append("<tr><td>" + review[i].getReview() + "</td><td>" + review[i].getReviewId() + "</td><td>" + review[i].getShopid() + "</td><td>" + review[i].getRating() + "</td></tr>");
+            }
+        } catch (Exception e) {
+            sb.append("</table><br>Error getting review: " + e.toString() + "<br>");
+        }
+        sb.append("</table></body></html>");
+       return sb.toString();
 
-//    @GET
-//    @Path("/{reviewId}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public String getReview(@PathParam("reviewId") int reviewId) throws IOException {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("<html><body><style>table, th, td {font-family:Arial,Verdana,sans-serif;font-size:16px;padding: 0px;border-spacing: 0px;}</style><b>MESSAGE LIST:</b><br><br><table cellpadding=10 border=1><tr><td>`name`</td><td>reviewId</td><td>shopId</td><td>rating</td></tr>");
-//        try {
-//            Model db = Model.singleton();
-//            Review[] review = db.getReview();
-//            for (int i = 0; i < review.length; i++) {
-//                sb.append("<tr><td>" + review[i].getReview() + "</td><td>" + review[i].getReviewId() + "</td><td>" + review[i].getShopid() + "</td><td>" + review[i].getRating() + "</td></tr>");
-//            }
-//        } catch (Exception e) {
-//            sb.append("</table><br>Error getting review: " + e.toString() + "<br>");
-//        }
-//        sb.append("</table></body></html>");
-//        return sb.toString();
-//    }
+    }
 
-//    @PUT
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public String updateCoffeeShop(String jobj) throws IOException {
-//        ObjectMapper mapper = new ObjectMapper();
-//        Review review = mapper.readValue(jobj.toString(), Review.class);
-//        StringBuilder text = new StringBuilder();
-//        try {
-//            Model db = Model.singleton();
-//            int reviewId = review.getReviewId();
-//            db.updateReview(review);
-//            logger.log(Level.INFO, "update contents with review id=" + reviewId);
-//            text.append("shop contents updated with review id=" + reviewId + "\n");
-//        } catch (SQLException sqle) {
-//            String errText = "Error updating review after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
-//            logger.log(Level.SEVERE, errText);
-//            text.append(errText);
-//        } catch (Exception e) {
-//            logger.log(Level.SEVERE, "Error connecting to db.");
-//            text.append("Error connecting to db.");
-//        }
-//        return text.toString();
-//
-//    }
+   @GET
+   @Path("/{reviewId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getReview(@PathParam("reviewId") int reviewId) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><body><style>table, th, td {font-family:Arial,Verdana,sans-serif;font-size:16px;padding: 0px;border-spacing: 0px;}</style><b>MESSAGE LIST:</b><br><br><table cellpadding=10 border=1><tr><td>`name`</td><td>reviewId</td><td>shopId</td><td>rating</td></tr>");
+        try {
+            Model db = Model.singleton();
+            Review[] review = db.getReview();
+           for (int i = 0; i < review.length; i++) {
+                sb.append("<tr><td>" + review[i].getReview() + "</td><td>" + review[i].getReviewId() + "</td><td>" + review[i].getShopid() + "</td><td>" + review[i].getRating() + "</td></tr>");
+           }
+       } catch (Exception e) {
+          sb.append("</table><br>Error getting review: " + e.toString() + "<br>");
+        }
+        sb.append("</table></body></html>");
+        return sb.toString();
+    }
 
-//    @POST
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public String createReview(String jobj) throws IOException {
-//        ObjectMapper mapper = new ObjectMapper();
-//        Review review = mapper.readValue(jobj.toString(), Review.class);
-//
-//        StringBuilder text = new StringBuilder();
-//        text.append("The JSON obj:" + jobj.toString() + "\n");
-//        text.append("review content is  " + review.getReview()+ "\n");
-//        text.append("review from coffeeshop ID is " + review.getShopid() + "\n");
-//        text.append("reviewId is " + review.getReviewId() + "\n");
-//        text.append("review rating is " + review.getRating() + "\n");
-//        
-//        try {
-//            Model db = Model.singleton();
-//            int reviewId = db.createReview(review);
-//            logger.log(Level.INFO, "Review persisted to db as id=" + reviewId);
-//            text.append("Review id persisted with id=" + reviewId);
-//        } catch (SQLException sqle) {
-//            String errText = "Error persisting shop after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
-//            logger.log(Level.SEVERE, errText);
-//            text.append(errText);
-//        } catch (Exception e) {
-//            logger.log(Level.SEVERE, "Error connecting to db.");
-//        }
-//
-//        return text.toString();
-//
-//    }
-//
-//    @DELETE
-//    @Produces(MediaType.TEXT_PLAIN)
-//    @Path("/{id}")
-//    public String deleteReview(@PathParam("id") String jobj) throws IOException {
-//        ObjectMapper mapper = new ObjectMapper();
-//        Review review = mapper.readValue(jobj.toString(), Review.class);
-//        StringBuilder text = new StringBuilder();
-//        try {
-//            Model db = Model.singleton();
-//            int reviewId = review.getReviewId();
-//            db.deleteReview(reviewId);
-//            logger.log(Level.INFO, "review deleted from db=" + reviewId);
-//            text.append("review id deleted with id=" + reviewId);
-//        } catch (SQLException sqle) {
-//            String errText = "Error deleteing review after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
-//            logger.log(Level.SEVERE, errText);
-//            text.append(errText);
-//        } catch (Exception e) {
-//            logger.log(Level.SEVERE, "Error connecting to db.");
-//            text.append("Error connecting to db.");
-//        }
-//        return text.toString();
-//    }
+    @PUT
+   @Produces(MediaType.APPLICATION_JSON)
+   @Consumes(MediaType.APPLICATION_JSON)
+   public String updateReview(String jobj) throws IOException {
+       ObjectMapper mapper = new ObjectMapper();
+       Review review = mapper.readValue(jobj.toString(), Review.class);
+        StringBuilder text = new StringBuilder();
+        try {
+           Model db = Model.singleton();
+          int reviewId = review.getReviewId();
+           db.updateReview(review);
+          logger.log(Level.INFO, "update contents with review id=" + reviewId);
+          text.append("shop contents updated with review id=" + reviewId + "\n");
+        } catch (SQLException sqle) {
+            String errText = "Error updating review after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+            logger.log(Level.SEVERE, errText);
+            text.append(errText);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error connecting to db.");
+            text.append("Error connecting to db.");
+        }
+       return text.toString();
+
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+   public String createReview(String jobj) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Review review = mapper.readValue(jobj.toString(), Review.class);
+
+        StringBuilder text = new StringBuilder();
+        text.append("The JSON obj:" + jobj.toString() + "\n");
+        text.append("review content is  " + review.getReview()+ "\n");
+        text.append("review from coffeeshop ID is " + review.getShopid() + "\n");
+        text.append("reviewId is " + review.getReviewId() + "\n");
+        text.append("review rating is " + review.getRating() + "\n");
+        
+        try {
+            Model db = Model.singleton();
+            int reviewId = db.createReview(review);
+            logger.log(Level.INFO, "Review persisted to db as id=" + reviewId);
+            text.append("Review id persisted with id=" + reviewId);
+        } catch (SQLException sqle) {
+           String errText = "Error persisting shop after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+            logger.log(Level.SEVERE, errText);
+            text.append(errText);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error connecting to db.");
+        }
+
+        return text.toString();
+    }
+
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{id}")
+    public String deleteReview(@PathParam("id") String jobj) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Review review = mapper.readValue(jobj.toString(), Review.class);
+        StringBuilder text = new StringBuilder();
+       try {
+            Model db = Model.singleton();
+           int reviewId = review.getReviewId();
+           db.deleteReview(reviewId);
+            logger.log(Level.INFO, "review deleted from db=" + reviewId);
+           text.append("review id deleted with id=" + reviewId);
+        } catch (SQLException sqle) {
+            String errText = "Error deleteing review after db connection made:\n" + sqle.getMessage() + " --- " + sqle.getSQLState() + "\n";
+           logger.log(Level.SEVERE, errText);
+            text.append(errText);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error connecting to db.");
+            text.append("Error connecting to db.");
+       }
+        return text.toString();
+    }
 
 }
