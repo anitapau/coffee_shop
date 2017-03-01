3ceb3a836742d7dd6601a0dae16718a28f0e29f3
Thanh Le <thanh07@uw.edu>
2017-03-01 00:36:57 -0800

    Create delete_review.sh

---
+++ src/scripts/delete_review.sh
@@ -0,0 +1,12 @@
+
+ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/review/1
+# heroku
+#ENDPOINT=https://frozen-temple-15527.herokuapp.com/tcss360/review/1
+# local web runner deploy
+# ENDPOINT=http://localhost:8080/tcss360/review/1
+# net beans deploy
+#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/review
+# manual deploy
+#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/review
+curl -X DELETE -H "Content-Type: application/json" $ENDPOINT
+echo
