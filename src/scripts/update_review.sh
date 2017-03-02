b4530df1ada7291700d7e0a4e04d36e56aaf37f2
Thanh Le <thanh07@uw.edu>
2017-03-01 19:12:30 -0800

    Update update_review.sh

--- src/scripts/update_review.sh
+++ src/scripts/update_review.sh
@@ -1,10 +1,9 @@
-
 ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/review
 # local web runner deploy
-#ENDPOINT=http://localhost:8080/tcss360/coffeeShop
+#ENDPOINT=http://localhost:8080/tcss360/review
 # net beans deploy
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/review
 # manual deploy
 #ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/review
-curl -X PUT -H "Content-Type: application/json" -d '{"name": "Updated Shop","description": "Updated seattle downtown","address": "Updated 123 test", "id":3}' $ENDPOINT
+curl -X PUT -H "Content-Type: application/json" -d '{"reviews": "It is good","reviewid": 1, "shopid": 1,"rating":4}' $ENDPOINT
 echo
