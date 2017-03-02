818c3e4100b2aa29d7c1d6a2631f3ca7bc7eb3bc
anitapau <anitapau@uw.edu>
2017-03-02 00:18:32 -0800

    Update update_review.sh

--- src/scripts/update_review.sh
+++ src/scripts/update_review.sh
@@ -5,5 +5,5 @@ ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/review
 #ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/review
 # manual deploy
 #ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/review
-curl -X PUT -H "Content-Type: application/json" -d '{"reviews": "It is good","reviewid": 1, "shopid": 1,"rating":4}' $ENDPOINT
+curl -X PUT -H "Content-Type: application/json" -d '{"review": "It is good","reviewid": 1, "shopid": 1,"rating":4}' $ENDPOINT
 echo
