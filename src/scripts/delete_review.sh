bfc742308a5b43690c2819cc1abc4b7d39a2decf
anitapau <anitapau@uw.edu>
2017-03-02 11:28:12 -0800

    Update delete_review.sh

--- src/scripts/delete_review.sh
+++ src/scripts/delete_review.sh
@@ -1,12 +1,12 @@
 
-ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/review/1
+ENDPOINT=https://gentle-coast-59786.herokuapp.com/tcss360/review/12
 # heroku
-#ENDPOINT=https://frozen-temple-15527.herokuapp.com/tcss360/review/1
+#ENDPOINT=https://frozen-temple-15527.herokuapp.com/tcss360/review/12
 # local web runner deploy
 # ENDPOINT=http://localhost:8080/tcss360/coffeeShop/1
 # net beans deploy
-#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/review/1
+#ENDPOINT=http://localhost:8084/sample_maven_web_app/tcss360/review/12
 # manual deploy
-#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/review/1
+#ENDPOINT=http://localhost:8080/sample_maven_web_app-1.0-SNAPSHOT/tcss360/review/12
 curl -X DELETE -H "Content-Type: application/json" $ENDPOINT
 echo
